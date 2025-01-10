bbf285280d773e9bdc0cb67362a2dbe8555ae3b1
Cameron Scott <casctt@clemson.edu>
2025-01-10 15:47:36 -0500

    setup

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Clog</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/camscott16',
   },
   chat: {
-    link: 'https://discord.com',
+    link: 'https://discord.gg/82XcaffD',
   },
   docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'Produced by the Clog Team :P',
   },
 }
 
